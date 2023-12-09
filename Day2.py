c89947f65c3ff700d57c1374440cbc8afaafdbc4
Anuj-Pradhan235 <pradhananuj235@gmail.com>
2023-12-09 12:46:13 +0530

    Done

---
+++ Day2.py
@@ -0,0 +1,6 @@
+print("Welcome to the tip calculator");
+amount=float(input("What was the total bill? $"));
+noOfPersons=int(input("How many people to split the bill? "));
+tip=int(input("What percentage tip would you like to give? 10,12, or 15? "));
+roundOfAmount=((amount+amount*(tip/100))/noOfPersons);
+print("Each person should pay: $"+ str( round( roundOfAmount,2)))
