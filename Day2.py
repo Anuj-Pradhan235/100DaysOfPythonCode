c9e8474cee558cf7c006e70fdf68780621367162
Anuj-Pradhan235 <pradhananuj235@gmail.com>
2023-12-09 13:40:01 +0530

    Done

--- Day2.py
+++ Day2.py
@@ -1,7 +1,25 @@
 # Tip Calculator
-print("Welcome to the tip calculator");
-amount=float(input("What was the total bill? $"));
-noOfPersons=int(input("How many people to split the bill? "));
-tip=int(input("What percentage tip would you like to give? 10,12, or 15? "));
-roundOfAmount=((amount+amount*(tip/100))/noOfPersons);
-print("Each person should pay: $"+ str( round( roundOfAmount,2)))
+# print("Welcome to the tip calculator");
+# amount=float(input("What was the total bill? $"));
+# noOfPersons=int(input("How many people to split the bill? "));
+# tip=int(input("What percentage tip would you like to give? 10,12, or 15? "));
+# roundOfAmount=((amount+amount*(tip/100))/noOfPersons);
+# print("Each person should pay: $"+ str( round( roundOfAmount,2)))
+
+#Data Types: Integers,Float,String,Boolean,Large Interger: 222,333,444 => 222_333_444 => 222333444
+# print("Hello"[4])
+# a=123
+# print(type(a));
+# print(str(11)+str(22));
+# b=float(a);
+# print(type(b));
+
+#BMI Calculator
+# height = input()
+# weight = input()
+# weight_as_int = int(weight)
+# height_as_float = float(height)
+# bmi = weight_as_int / height_as_float ** 2
+# bmi = weight_as_int / (height_as_float * height_as_float)
+# bmi_as_int = int(bmi)
+# print(bmi_as_int)
