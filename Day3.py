9712032d7d38208042eea9d6973bd719ccaf6251
Anuj-Pradhan235 <pradhananuj235@gmail.com>
2023-12-11 01:20:31 +0530

    Done

--- Day3.py
+++ Day3.py
@@ -1,7 +1,14 @@
 #Coditional Statement
-print("Welcome to Rollercoster!")
-height=int(input("What is your height in cm ? "))
-if(height>120):
-    print("Can ride")
-else :
-    print("Can't ride")
+# print("Welcome to Rollercoaster!")
+# height=int(input("What is your height in cm? "))
+# if(height>120):
+#     print("Can ride")
+# else :
+#     print("Can't ride")
+
+#Odd or Even
+number=int(input("Enter the number:"));
+if(number%2==0):
+    print("The given number is even");
+else:
+    print("The given number is odd");
